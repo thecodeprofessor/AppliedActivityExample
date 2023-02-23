72d983830fd9b3b35b55e628fb36bced45affd2d
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-02-23 12:11:19 -0500

    Added Flyout menu and page routing.

--- AppliedActivity3/AppShell.xaml.cs
+++ AppliedActivity3/AppShell.xaml.cs
@@ -5,5 +5,11 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+
+        Routing.RegisterRoute(nameof(Views.MainPage), typeof(Views.MainPage));
+        Routing.RegisterRoute(nameof(Views.StudentPage), typeof(Views.StudentPage));
+        Routing.RegisterRoute(nameof(Views.StudentsPage), typeof(Views.StudentsPage));
+        Routing.RegisterRoute(nameof(Views.CoursePage), typeof(Views.CoursePage));
+        Routing.RegisterRoute(nameof(Views.CoursesPage), typeof(Views.CoursesPage));
+    }
 }
