74f1f7b6308c949220a93682dc3a507d10245bdf
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-02-23 11:51:38 -0500

    Added the student and courses models.

---
+++ AppliedActivity3/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace AppliedActivity3.Models
+{
+    [Table("Courses")]
+    public class Course
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+        public string CRN { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AppliedActivity3/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace AppliedActivity3.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Number { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
